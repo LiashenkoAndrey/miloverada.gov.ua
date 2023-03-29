0508d85fdc2f61f0878cd9d00c3d5eb0e4b0c7d3
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-03-29 11:13:00 +0300

    remove tests body to prevent unwanted results

--- src/test/java/gov/milove/repositories/UserTest.java
+++ src/test/java/gov/milove/repositories/UserTest.java
@@ -1,20 +1,10 @@
 package gov.milove.repositories;
 
-
-import gov.milove.domain.News;
-import gov.milove.domain.Role;
-import gov.milove.domain.User;
-import jakarta.persistence.EntityExistsException;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,19 +17,7 @@ public class UserTest {
 
     @Test
     public void test() {
-        User user = userRepo.findByLogin("HelloWorld_").orElseThrow(EntityNotFoundException::new);
-        System.out.println(user.getAuthority());
-        System.out.println(user.getAuthorities());
-//        User user = new User();
-//        user.setFirst_name("Admin");
-//        user.setLast_name("Admin");
-//        user.setLogin("HelloWorld_");
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//        user.setPassword(encoder.encode("__milove@_VG1984"));
-//        List<Role> roles = new ArrayList<>();
-//        roles.add(new Role("ADMIN"));
-//        user.setAuthority(roles);
-//        userRepo.save(user);
+
     }
 
 }
