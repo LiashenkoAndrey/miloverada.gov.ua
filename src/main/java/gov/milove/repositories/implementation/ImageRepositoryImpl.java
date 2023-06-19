12b5502e58d794c564fff9e2b89c5d14e88fb986
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-06-19 22:05:24 +0300

    add last_queried field to image

--- src/main/java/gov/milove/repositories/implementation/ImageRepositoryImpl.java
+++ src/main/java/gov/milove/repositories/implementation/ImageRepositoryImpl.java
@@ -1,20 +1,28 @@
 package gov.milove.repositories.implementation;
 
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.result.UpdateResult;
+import gov.milove.exceptions.ImageNotFoundException;
+import gov.milove.exceptions.ImageRepositoryException;
 import gov.milove.repositories.ImageRepository;
-import jakarta.persistence.EntityExistsException;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.bson.types.Binary;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.Optional;
+import javax.imageio.*;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Iterator;
 
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.combine;
@@ -23,11 +31,14 @@ import static com.mongodb.client.model.Updates.set;
 @Controller
 public class ImageRepositoryImpl implements ImageRepository {
 
-    private final MongoDatabase mongoDatabase;
+    private static final Logger logger = LoggerFactory.getLogger(ImageRepositoryImpl.class);
 
-    public ImageRepositoryImpl(MongoDatabase mongoDatabase) {
-        this.mongoDatabase = mongoDatabase;
+    private final MongoCollection<Document> collection;
+
+    public ImageRepositoryImpl(@Qualifier("imageCollection") MongoCollection<Document> collection) {
+        this.collection = collection;
     }
+
 
     /**
      * This method saves image to mongoDB and returns id of image in database
@@ -35,71 +46,66 @@ public class ImageRepositoryImpl implements ImageRepository {
      */
     @Override
     public String saveImage(MultipartFile file) {
-
-        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("milove_images");
-        Document document = new Document();
         String id;
         try {
-            document.append("filename", file.getName());
-            document.append("binary_image", file.getBytes());
-            mongoCollection.insertOne(document);
-            id = mongoCollection.find(document).iterator().tryNext().get("_id").toString();
-            System.out.println("image saved: " + id);
-        } catch (IOException ex) {
-            System.out.println("Error saving image!");
-            return "error";
-        }
+            Document document = new Document()
+                .append("filename", file.getName())
+                .append("binary_image", file.getBytes())
+                .append("last_queried", LocalDate.now());
 
+            collection.insertOne(document);
+            Document savedImage = collection.find(document).first();
+
+            if (savedImage != null) {
+                id = savedImage.get("_id").toString();
+            } else throw new IOException("can't find document with filename:" + file.getName());
+
+        } catch (IOException e) {
+            logger.error(e.toString());
+            throw new ImageRepositoryException(e);
+        }
 
         return id;
     }
 
     /**
-     * @param image_id
-     * @param file
-     * @return
+     * @param image_id image id
+     * @param file file
      */
     @Override
     public void updateImage(String image_id, MultipartFile file) {
-        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("milove_images");
-        byte[] binary;
         try {
-            binary = file.getBytes();
-        }catch (IOException ex) {
-            System.out.println("error");
-            throw new RuntimeException(ex);
-        }
+            collection.updateOne(
+                    eq("_id", new ObjectId(image_id)),
+                    combine(
+                            set("binary_image", file.getBytes()),
+                            set("filename", file.getName()),
+                            set("last_queried", LocalDate.now())
+                    )
+            );
 
-        Bson filter = eq("_id", new ObjectId(image_id));
-        Bson updateOperation = combine(set("binary_image", binary), set("filename", file.getName()));
-        UpdateResult updateResult = mongoCollection.updateOne(filter, updateOperation);
-        System.out.println(updateResult);
+        } catch (IOException e) {
+            logger.error(e.toString());
+            throw new ImageRepositoryException(e);
+        }
     }
 
     @Override
     public byte[] getImageById(String id) {
-        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("milove_images");
-        FindIterable<Document> findIterable = mongoCollection.find(new Document("_id", new ObjectId(id)));
-        Document document = Optional.ofNullable(findIterable.first()).orElseThrow(EntityExistsException::new);
-        Binary binary = (Binary) document.get("binary_image");
-        return binary.getData();
+        Document document = collection.findOneAndUpdate(
+                new Document("_id", new ObjectId(id)),
+                set("last_queried", LocalDate.now())
+        );
+
+        if (document != null) {
+            return ((Binary) document.get("binary_image")).getData();
+        } else {
+            throw new ImageNotFoundException("image with id: " + id +" not found!");
+        }
     }
 
     public void deleteImageById(String image_id) {
-        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("milove_images");
-        mongoCollection.deleteOne(new Document("_id", new ObjectId(image_id)));
-        System.out.println("Image with _id "+ image_id +" was deleted!");
+        collection.deleteOne(new Document("_id", new ObjectId(image_id)));
     }
 
-    private Document prepareDocument(MultipartFile file) {
-        Document document = new Document();
-        try {
-            document.append("binary_image", file.getBytes());
-            document.append("filename", file.getName());
-        } catch (IOException e) {
-            System.out.println("Fail to save image: " + file.getName());
-            throw new RuntimeException(e);
-        }
-        return document;
-    }
 }
